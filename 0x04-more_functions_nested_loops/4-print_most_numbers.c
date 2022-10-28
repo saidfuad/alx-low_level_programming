5f1d6e724da66823a52da5d9a2421403ccee320e
Said Fuad Said <saidfuad91@gmail.com>
2022-10-28 10:53:55 +0300

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers 
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+			continue;
+		else
+			_putchar(num);
+	}
+	_putchar('\n');
+}
