7c4aed478425e116e256c13c0100ca4aa4ced2cc
Said Fuad Said <saidfuad91@gmail.com>
2022-10-28 10:52:49 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers 
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
