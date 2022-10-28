c9a47ec419f86155a871d2aae8d035a664c1799e
Said Fuad Said <saidfuad91@gmail.com>
2022-10-28 10:49:17 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _isupper.
- * @c: Character input
- * Return: Always 0.
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 1 or 0
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 65) && (c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
